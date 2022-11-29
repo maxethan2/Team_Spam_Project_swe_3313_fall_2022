3ee2ce0fa4ca84c14273fd96e88f165740548974
Max Haviv <me03haviv@gmail.com>
2022-11-28 23:00:45 -0500

    Added csv extract funstionality
    Added nuget package csvhelper, added button to form management, added functionality to extract csv in form management and CsvExtractLines.cs to help with formatting

--- Source/CoffeePointOfSale/Forms/FormManagement.Designer.cs
+++ Source/CoffeePointOfSale/Forms/FormManagement.Designer.cs
@@ -28,65 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtDeleteThis = new System.Windows.Forms.TextBox();
-            this.lblRemoveThis = new System.Windows.Forms.Label();
             this.CancelButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // txtDeleteThis
-            // 
-            this.txtDeleteThis.AcceptsReturn = true;
-            this.txtDeleteThis.Location = new System.Drawing.Point(234, 137);
-            this.txtDeleteThis.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.txtDeleteThis.Multiline = true;
-            this.txtDeleteThis.Name = "txtDeleteThis";
-            this.txtDeleteThis.Size = new System.Drawing.Size(687, 124);
-            this.txtDeleteThis.TabIndex = 1;
-            // 
-            // lblRemoveThis
-            // 
-            this.lblRemoveThis.AutoSize = true;
-            this.lblRemoveThis.Location = new System.Drawing.Point(234, 108);
-            this.lblRemoveThis.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.lblRemoveThis.Name = "lblRemoveThis";
-            this.lblRemoveThis.Size = new System.Drawing.Size(212, 25);
-            this.lblRemoveThis.TabIndex = 2;
-            this.lblRemoveThis.Text = "Demo - Remove This Box";
             // 
             // CancelButton
             // 
             this.CancelButton.BackColor = System.Drawing.Color.Red;
             this.CancelButton.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.CancelButton.ForeColor = System.Drawing.Color.Snow;
-            this.CancelButton.Location = new System.Drawing.Point(1123, 658);
+            this.CancelButton.Location = new System.Drawing.Point(786, 395);
+            this.CancelButton.Margin = new System.Windows.Forms.Padding(2);
             this.CancelButton.Name = "CancelButton";
-            this.CancelButton.Size = new System.Drawing.Size(192, 50);
+            this.CancelButton.Size = new System.Drawing.Size(134, 30);
             this.CancelButton.TabIndex = 3;
             this.CancelButton.Text = "Cancel";
             this.CancelButton.UseVisualStyleBackColor = false;
             this.CancelButton.Click += new System.EventHandler(this.CancelButton_Click);
             // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(82)))), ((int)(((byte)(94)))));
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.Color.Snow;
+            this.button1.Location = new System.Drawing.Point(339, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(273, 182);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Extract CSV";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
             // FormManagement
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(39)))), ((int)(((byte)(42)))));
-            this.ClientSize = new System.Drawing.Size(1344, 712);
+            this.ClientSize = new System.Drawing.Size(941, 427);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.CancelButton);
-            this.Controls.Add(this.lblRemoveThis);
-            this.Controls.Add(this.txtDeleteThis);
-            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "FormManagement";
             this.Text = "FormManagement";
             this.Load += new System.EventHandler(this.OnLoadFormManagement);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-        private TextBox txtDeleteThis;
-        private Label lblRemoveThis;
         private Button CancelButton;
+        private Button button1;
     }
 }
---
+++ Source/CoffeePointOfSale/Services/CsvExtract/CsvExtractLine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeePointOfSale.Services.CsvExtract
+{
+    public class CsvExtractLine
+    {
+        public string? CustomerId { get; set; }
+
+        public string? CustomerPhoneNumber { get; set; }
+
+        public string? CustomerFirstName { get; set; }
+
+        public string? CustomerLastName { get; set; }
+
+        public int? CustomerRewardsPoints { get; set; }
+
+        public string? OrderDate { get; set; }
+
+        public decimal? OrderTotal { get; set; }
+
+        public decimal? OrderSubtotal { get; set; }
+
+        public decimal? OrderTax { get; set; }
+
+        public string? OrderDetail { get; set; }
+    }
+}
