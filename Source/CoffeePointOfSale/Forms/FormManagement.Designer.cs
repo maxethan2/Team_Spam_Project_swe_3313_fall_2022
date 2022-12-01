2519f42a51df6dce1d91885e2132886e51914a90
Weaton1 <114958787+Weaton1@users.noreply.github.com>
2022-12-01 18:09:09 -0500

    Made the CSV design

--- Source/CoffeePointOfSale/Forms/FormManagement.Designer.cs
+++ Source/CoffeePointOfSale/Forms/FormManagement.Designer.cs
@@ -30,6 +30,9 @@
         {
             this.CancelButton = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // CancelButton
@@ -50,14 +53,34 @@
             // 
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(82)))), ((int)(((byte)(94)))));
             this.button1.Font = new System.Drawing.Font("Segoe UI", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button1.ForeColor = System.Drawing.Color.Snow;
-            this.button1.Location = new System.Drawing.Point(339, 69);
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(445, 189);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(273, 182);
+            this.button1.Size = new System.Drawing.Size(457, 363);
             this.button1.TabIndex = 4;
             this.button1.Text = "Extract CSV";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(82)))), ((int)(((byte)(94)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.ForeColor = System.Drawing.SystemColors.Control;
+            this.panel1.Location = new System.Drawing.Point(52, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1234, 61);
+            this.panel1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(382, -11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(500, 72);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Management Menu";
             // 
             // FormManagement
             // 
@@ -65,12 +88,15 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(39)))), ((int)(((byte)(42)))));
             this.ClientSize = new System.Drawing.Size(1348, 721);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.CancelButton);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "FormManagement";
             this.Text = "FormManagement";
             this.Load += new System.EventHandler(this.OnLoadFormManagement);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -78,5 +104,7 @@
         #endregion
         private Button CancelButton;
         private Button button1;
+        private Panel panel1;
+        private Label label1;
     }
 }
